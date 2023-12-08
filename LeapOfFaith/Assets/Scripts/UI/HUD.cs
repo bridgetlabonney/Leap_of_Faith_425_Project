a1903969a9cd5bbcf7451e27183cf27d3b47b704
Bridget LaBonney <blabonney@nevada.unr.edu>
2023-12-07 22:19:39 -0800

    Update HUD.cs

--- LeapOfFaith/Assets/Scripts/UI/HUD.cs
+++ LeapOfFaith/Assets/Scripts/UI/HUD.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class HUD : MonoBehaviour
 {
+    //controls health bar
+    public GameObject h1;
+    public GameObject h2;
+    public GameObject h3;
+    public Player player;
+    public Sprite half;
+    public Sprite empty;
     // Start is called before the first frame update
     void Start()
     {
-        
+       // h1.GetComponent<UnityEngine.UI.Image>().sprite
     }
 
     // Update is called once per frame
     void Update()
     {
         //check for new updates to player values
+       
+    }
+
+    void healthConfig(double health)
+    {
+       
+
+
     }
 }
