f3e5e3c5af55de2e7a9b2fff4843b7c9573f45fd
bgalvin1 <bgalvin@nevada.unr.edu>
2024-04-07 13:35:15 -0700

    Teleport Mechanic added
    A basic teleporter has been implemented. It teleports the player to a set destination (can be another object) with an offset of -2 in the x axis (to prevent double teleport)

---
+++ LeapOfFaith/Assets/Scripts/Features/teleport.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+    [SerializeField] GameObject destination;
+    //Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = Player.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("tag succesful");
+            //grab position of destination
+            Vector3 des = destination.transform.position;
+            StartCoroutine(Teleport(des));
+            //set position of player to position of destination
+
+        }
+    }
+    IEnumerator Teleport(Vector3 des)
+    {
+        Debug.Log("Coroutine");
+        Player.SetActive(false);
+        yield return new WaitForSeconds(0.01f);
+        Player.transform.position = des + new Vector3(-2f, 0f); //Position of destination
+        yield return new WaitForSeconds(0.01f);
+        Player.SetActive(true);
+    }
+}
