6a68feacb97344bd4d6d2ee16d949acfc4845cdf
Bridget LaBonney <blabonney@nevada.unr.edu>
2024-04-30 15:01:03 -0700

    Revert "dialouge prompter"
    This reverts commit 609720afa03d406e8218d37cb723436f94c20230.

--- LeapOfFaith/Assets/Scripts/UI/DialogueBox.cs
+++ LeapOfFaith/Assets/Scripts/UI/DialogueBox.cs
@@ -13,7 +13,7 @@ using UnityEngine.SceneManagement;
 public class DialogueBox : MonoBehaviour
 {
     public Dialogue d;
-    public dialougeprompt pmt;
+    
     public Text dialogue;
     private int txtcnt = 0;
     public bool SceneChange;
@@ -57,13 +57,12 @@ public class DialogueBox : MonoBehaviour
 
     void changeDialogue(Dialogue newWords)
     {
-       
         d = newWords;
     }
     IEnumerator typewriter(string t)
     {
 
-              
+            
               string holder = null;
               foreach(char c in t)
               {
@@ -73,24 +72,20 @@ public class DialogueBox : MonoBehaviour
                 yield return new WaitForSeconds(3 / 4);
 
               }
-        pmt.waiting = false;
 
-    }
+        }
 
     //displays each dialogue box
     IEnumerator displaywords(string[] words)
     {
-        
+
         foreach (string t in words)
         {
             txtcnt++;
-            pmt.waiting = true;
             StartCoroutine(typewriter(t));
             if (!Tutorial)
             {
-                
                 yield return new WaitUntil(() => (Input.GetMouseButtonDown(0) || Input.GetButtonDown("Jump")));
-                
             }
             else
             {
